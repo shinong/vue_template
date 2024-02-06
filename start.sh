3d4afbb1b071e6f07b9c49a45ae92b28aef0fc9f
Shinong <shinongmao@gmail.com>
2024-02-06 15:54:57 +0800

    fix docker error

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 app="frontend"
-docker build -t ${app} .
-docker run -d --name "Frontend" -p 8080:80 ${app}
+docker build -t "frontend" .
+docker run -d --name "Frontend" -p 8080:80 "frontend"
