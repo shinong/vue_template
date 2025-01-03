7d417a78b2bc772b27b7314a12f49939494518e6
Shinong <shinongmao@gmail.com>
2025-01-03 15:44:23 -0500

    cleaning up

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-app="frontend"
-docker build -t "frontend" .
-docker run -d --name "Frontend" -p 8080:80 "frontend"
--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [vue(), vueJsx()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    define: {
-      'process.env': {
-        VITE_ENV: mode // Pass the mode to the app
-      }
-    },
-    base: mode === 'test' ? '/test/' : '/',
-    server: {
-      port: 4430
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 4430
   }
 })
